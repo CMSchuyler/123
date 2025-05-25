984f2e0db70a4370fa28fcb3eb6f494a20cb9cbd
Shujun Tian <2777606511@qq.com>
2025-05-25 12:25:14 +0800

    Update Scene environment color

--- src/gallery/Scene.jsx
+++ src/gallery/Scene.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Gallery from './Gallery';
 import Ocean from './Ocean';
 import Fog from './Fog';
-import { Environment, OrbitControls } from '@react-three/drei';
+import { Environment } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { useEffect } from 'react';
+import { EffectComposer, Bloom, DepthOfField, Noise } from '@react-three/postprocessing';
+import { BlendFunction } from 'postprocessing';
 
 const Scene = () => {
 	return (
@@ -16,12 +17,30 @@ const Scene = () => {
 				rotation: [-Math.PI * 0.5, 0, 0],
 			}}
 		>
-			{/* <OrbitControls /> */}
-			<color attach="background" args={['#191920']} />
+			<color attach="background" args={['#E3BEA1']} />
 			<Gallery />
 			<Ocean />
 			<Fog />
 			<Environment files={'./textures/preller_drive_1k.hdr'} />
+			
+			<EffectComposer>
+				<DepthOfField 
+					focusDistance={0.01} 
+					focalLength={0.2} 
+					bokehScale={3} 
+				/>
+				<Bloom 
+					intensity={0.5} 
+					luminanceThreshold={0.1} 
+					luminanceSmoothing={0.9} 
+					height={300}
+				/>
+				<Noise 
+					opacity={0.8}
+					blendFunction={BlendFunction.OVERLAY}
+					premultiply
+				/>
+			</EffectComposer>
 		</Canvas>
 	);
 };
